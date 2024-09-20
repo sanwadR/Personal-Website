81f3b1c3031bcdf02a18c453682f287aa50e5b70
Sanwad Rashikar <sanwadrashinkar8888@gmail.com>
2024-09-20 17:10:20 +0530

    Add aboutme.jpg

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
-import project5 from "../assets/projects/pokedex.png";
+import project5 from "../assets/projects/Pokedex.png";
 import project6 from "../assets/projects/WanderLust.png";
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications.  I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
